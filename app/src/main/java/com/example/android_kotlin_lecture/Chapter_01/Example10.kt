57b89087a2b559ca244d59214ac4d0a121251648
Dahoonkk <iamkdh1@naver.com>
2023-05-08 23:16:57 +0900

    study commit

---
+++ app/src/main/java/com/example/android_kotlin_lecture/Chapter_01/Example10.kt
@@ -0,0 +1,14 @@
+package com.example.android_kotlin_lecture.Chapter_01
+
+// 10. String Templates
+
+fun main() {
+    val a = 10
+    val name = "hello"
+    val isHigh = true
+
+    println(a.toString() + name + isHigh.toString()) // 자바의 방식
+
+    // 코틀린의 String Templates
+    println("$a ${name.length} $isHigh") // $를 활용하여 간단하게 형식을 지정해서 출력할 수 있음
+}
